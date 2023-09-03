b7bad40e3d3b033c2fd9e777b5b8b3bbc72f7c67
aosman21212 <a.osman@bab.com.sa>
2023-09-03 12:48:23 +0300

    Add your commit

---
+++ limesurvey/application/config/security.php
@@ -0,0 +1,23 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/*
+ * LimeSurvey
+ * Copyright (C) 2007-2019 The LimeSurvey Project Team / Carsten Schmitz
+ * All rights reserved.
+ * License: GNU/GPL License v3 or later, see LICENSE.php
+ * LimeSurvey is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+
+/* 
+WARNING!!!
+ONCE SET, ENCRYPTION KEYS SHOULD NEVER BE CHANGED, OTHERWISE ALL ENCRYPTED DATA COULD BE LOST !!!
+
+*/
+
+$config = array();
+$config['encryptionnonce'] = 'b9cc95c073527c760ab7df036a5df7e61c1acd1f72b7c5b5';
+$config['encryptionsecretboxkey'] = '358c9b40b13c1486796fae43d04445df2d1eb35eb349faefbb5d2212188689a8';
+return $config;
